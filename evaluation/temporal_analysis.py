ff0902311b4d8604073942500e33be043b665fd4
Zuzanna <zuzanna@swiftera.co>
2019-03-05 22:00:01 +0100

    fix in plotting iou

--- evaluation/temporal_analysis.py
+++ evaluation/temporal_analysis.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-
 def plotIoU(IoUFrames, dir):
     if not os.path.exists(dir):
         os.makedirs(dir)
@@ -12,24 +11,28 @@ def plotIoU(IoUFrames, dir):
     plt.xlabel("#frame")
     plt.savefig(dir+"/TP_overframe")
     # plt.show()
+    plt.close()
 
     plt.plot(IoUFrames[:,1])
     plt.ylabel("FN")
     plt.xlabel("#frame")
     plt.savefig(dir+"/FN_overframe")
     # plt.show()
+    plt.close()
 
     plt.plot(IoUFrames[:,2])
     plt.ylabel("FP")
     plt.xlabel("#frame")
     plt.savefig(dir+"/FP_overframe")
     # plt.show()
+    plt.close()
 
     plt.plot(IoUFrames[:,3])
     plt.ylabel("IoU")
     plt.xlabel("#frame")
     plt.savefig(dir+"/IoU_overframe")
     # plt.show()
+    plt.close()
 
 
 def plotF1(F1_frames, dir):
@@ -41,15 +44,18 @@ def plotF1(F1_frames, dir):
     plt.xlabel("#frame")
     plt.savefig(dir+"/F1_overframe")
     # plt.show()
+    plt.close()
 
     plt.plot(F1_frames[:,1])
     plt.ylabel("precision")
     plt.xlabel("#frame")
     plt.savefig(dir+"/precision_overframe")
     # plt.show()
+    plt.close()
 
     plt.plot(F1_frames[:,2])
     plt.ylabel("recall")
     plt.xlabel("#frame")
     plt.savefig(dir+"/recall_overframe")
     # plt.show()
+    plt.close()
