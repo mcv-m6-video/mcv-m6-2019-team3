54b3036d5a2a647122b853a4f0ce285f9f5e4267
pablorodriper <pablorodriper@gmail.com>
2019-03-12 01:09:10 +0100

    Merge gaussian with state-of-the-art

--- week2/main.py
+++ week2/main.py
@@ -1,9 +1,12 @@
-from processing.background_subtraction import BackgroundSubtractor
+from processing.background_subtraction import BackgroundSubtractor, single_gaussian_model
 
 video_path = "../datasets/AICity_data/train/S03/c010/vdo.avi"
 groundtruth_xml_path = "../annotations/m6-full_annotation.xml"
 
 if __name__ == "__main__":
+
+    # Gaussian modelling
+    single_gaussian_model(video_path, alpha=2.5, rho=1, adaptive=True)
 
     # State-of-the-art background subtractors
     BackgroundSubtractor(video_path, export_frames=True)
--- week2/processing/background_subtraction.py
+++ week2/processing/background_subtraction.py
@@ -1,6 +1,79 @@
 import cv2
 import os
 
+import numpy as np
+
+#from week1.utils.reading import read_annotations_file
+
+def get_pixels_single_gaussian_model(video_path, last_frame=int(2141*0.25)):
+    capture = cv2.VideoCapture(video_path)
+    n_frame = 0
+
+    while capture.isOpened() and n_frame <= last_frame:
+        valid, image = capture.read()
+        if not valid:
+            break
+        if n_frame==0:
+            gaussians = np.zeros((image.shape[0], image.shape[1], last_frame+1))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        gaussians[:, :, n_frame] = image
+
+        # Get groundtruth and detections from frame n
+        #gt_on_frame = [x for x in groundtruth_list if x.frame == n]
+        #gt_bboxes = [o.bbox for o in gt_on_frame]
+
+        n_frame += 1
+
+    gauss_mean = gaussians.mean(axis=2)
+    gauss_std = gaussians.std(axis=2)
+
+    return gauss_mean, gauss_std
+
+
+def get_frame_mask_single_gaussian_model(img, model_mean, model_std, alpha):
+    foreground = (abs(img - model_mean) >= alpha*(model_std+2))
+
+    return foreground
+
+
+def get_fg_mask_single_gaussian_model(video_path, first_frame, model_mean, model_std, alpha, rho, adaptive=False):
+    capture = cv2.VideoCapture(video_path)
+    n_frame = 0
+
+    while capture.isOpened():
+        valid, image = capture.read()
+        if not valid:
+            break
+        if n_frame == first_frame:
+            foreground = np.zeros((image.shape[0], image.shape[1], 2141 - first_frame))
+        if n_frame > first_frame:
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            foreground[:, :, n_frame-first_frame-1] = get_frame_mask_single_gaussian_model(image, model_mean, model_std, alpha)
+
+            if adaptive:
+                model_mean = rho*image + (1-rho)*model_mean
+                model_std = np.sqrt(rho*(image - model_mean)**2 + (1-rho)*model_std**2)
+
+        n_frame +=1
+
+    return foreground
+
+
+def single_gaussian_model(video_path, alpha, rho, adaptive=False):
+    print('Computing Gaussian model...')
+    mean, std = get_pixels_single_gaussian_model(video_path)
+    print('Gaussian computed for pixels')
+    print('Extracting Background...')
+    bg = get_fg_mask_single_gaussian_model(video_path, first_frame=int(2141 * 0.25), model_mean=mean, model_std=std,
+                                            alpha=alpha, rho=rho, adaptive=adaptive)
+    print('Extracted background with shape {}'.format(bg.shape))
+
+
+############################################
+########### State-of-the-art methods
+############################################
+
 def BackgroundSubtractor(video_path, export_frames=False):
     capture = cv2.VideoCapture(video_path)
     fgbg_MOG = cv2.bgsegm.createBackgroundSubtractorMOG()
@@ -33,10 +106,6 @@ def BackgroundSubtractor(video_path, export_frames=False):
 
     capture.release()
     cv2.destroyAllWindows()
-
-############################################
-########### State-of-the-art methods
-############################################
 
 def BackgroundSubtractorMOG(fgbg_MOG, frame, i, export_frames=False):
     fgmask = fgbg_MOG.apply(frame)
