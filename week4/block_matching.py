Fix image rotation

week4/block_matching.py
@@ -59,4 +59,5 @@ def block_matching_optical_flow(image1, image2, block_size=4, search_area=4, ste
                 optical_flow[j+displ_j:j+displ_j + block_size, i+displ_i:i+displ_i + block_size, 1] = -displ_j
     finish = timeit.default_timer()
     print('Time: {}'.format(finish-start))
-    return optical_flow
+
+    return np.transpose(optical_flow,(1,0,2))

week4/main.py
@@ -32,7 +32,7 @@ if __name__ == '__main__':
     step_size = [16]
     error_function=['SSD', 'MSD', 'SAD']
 
-    """
+
     for bs, sa, ss, ef in list(product(block_size, search_area, step_size, error_function)):
 
         print('Block size: {0}, search area: {1}, step size: {2}, error function: {3}'.format(bs,sa,ss,ef))
@@ -51,11 +51,11 @@ if __name__ == '__main__':
         #                 save_path=save_path + 'BM_optflow/')
         #plot_opticalflow_gt(gt_opticalflow, first_sequence, title='GT optical flow',
         #                 save_path=save_path + 'GT_optflow/', need_conversion=True)
-    """
+
 
     # Task 1.2
     farneback = farneback(sequences[0], sequences[1])
-    msen, pepn = compute_msen(flow_gt, np.rot90(farneback))
+    msen, pepn = compute_msen(flow_gt, farneback)
 
     print("MSEN: {}".format(msen))
     print("PEPN: {}".format(pepn))

week4/utils/optical_flow_metrics.py
@@ -34,9 +34,9 @@ def read_flow_data(gt_noc, test):
         fv_gt = (img_gt[:, :, 1] - 2. ** 15) / 64
         valid_gt = img_gt[:,:,0]
 
-        flow_gt = np.transpose(np.array([fu_gt, fv_gt, valid_gt]))
+        flow_gt = np.transpose(np.array([fu_gt, fv_gt, valid_gt]),(1,2,0))
 
-        flow_test = np.transpose(np.array([fu_test, fv_test, valid_test]))
+        flow_test = np.transpose(np.array([fu_test, fv_test, valid_test]),(1,2,0))
         print(flow_test[:-1].shape)
         plt.figure(1)
         plt.imshow(flow_gt)
@@ -61,7 +61,7 @@ def compute_msen(flow_gt, flow_test, threshold=3):
     flow_err[valid_gt == 0] = 0 
 
     plt.figure(1)
-    plt.imshow(np.transpose(flow_err), cmap="jet")
+    plt.imshow(flow_err, cmap="jet")
     plt.colorbar()
     plt.tick_params(axis='both', labelbottom=False, labelleft=False)
     plt.savefig('results/err_flow.png')