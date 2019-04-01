7898dcc690fe20fe5a23da90d65987c1157be052
pablorodriper <pablorodriper@gmail.com>
2019-04-01 12:49:19 +0200

    Change nested for

--- week4/main.py
+++ week4/main.py
@@ -1,3 +1,4 @@
+from itertools import product
 import numpy as np
 
 from utils.optical_flow_metrics import compute_msen, read_flow_data
@@ -29,23 +30,23 @@ if __name__ == '__main__':
     step_size = [16]
     error_function=['SSD', 'MSD', 'SAD']
 
-    for bs in block_size:
-        for sa in search_area:
-            for ss in step_size:
-                for ef in error_function:
-                    print('Block size: {0}, search area: {1}, step size: {2}, error function: {3}'.format(bs,sa,ss,ef))
-                    opticalflow = block_matching_optical_flow(sequences[0], sequences[1], block_size=bs, search_area=sa, step_size=ss, error_function=ef, compensation='Forward')
+    for bs, sa, ss, ef in list(product(block_size, search_area, step_size, error_function)):
 
-                    msen, pepn = compute_msen(flow_gt, opticalflow)
+        print('Block size: {0}, search area: {1}, step size: {2}, error function: {3}'.format(bs,sa,ss,ef))
+        opticalflow = block_matching_optical_flow(sequences[0], sequences[1], block_size=bs, search_area=sa, step_size=ss, error_function=ef, compensation='Forward')
 
-                    print("MSEN: {}".format(msen))
-                    print("PEPN: {}".format(pepn))
+        msen, pepn = compute_msen(flow_gt, opticalflow)
 
-                    opticalflow_images = read_opticalflow(opticalflow_path)
-                    gt_opticalflow = read_opticalflow(gt_path)
-                    first_sequence = read_sequences(sequences_path)
+        print("MSEN: {}".format(msen))
+        print("PEPN: {}".format(pepn))
 
-                    plot_opticalflow_bm(opticalflow, first_sequence, title='block matching',
-                                     save_path=save_path + 'BM_optflow/')
-                    plot_opticalflow_gt(gt_opticalflow, first_sequence, title='GT optical flow',
-                                     save_path=save_path + 'GT_optflow/', need_conversion=True)
+        opticalflow_images = read_opticalflow(opticalflow_path)
+        gt_opticalflow = read_opticalflow(gt_path)
+        first_sequence = read_sequences(sequences_path)
+
+        #plot_opticalflow_bm(opticalflow, first_sequence, title='block matching',
+        #                 save_path=save_path + 'BM_optflow/')
+        #plot_opticalflow_gt(gt_opticalflow, first_sequence, title='GT optical flow',
+        #                 save_path=save_path + 'GT_optflow/', need_conversion=True)
+
+
