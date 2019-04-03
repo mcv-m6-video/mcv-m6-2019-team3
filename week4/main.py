8198b119621462b214ab01bdfc086076fc094656
gemma <gemma@vilynx.com>
2019-04-03 14:32:22 +0200

    improvements in optional task, track by detection, optical flow plot

--- week4/main.py
+++ week4/main.py
@@ -30,37 +30,37 @@ save_path = 'plots/'
 
 if __name__ == '__main__':
 
-    # Task 1.1
-    create_folder(save_path + 'BM_optflow/')
-    create_folder(save_path + 'GT_optflow/')
-
-    sequences = read_sequences(sequences_path)
-    flow_gt, flow_test = read_flow_data(gt_noc, test)
-
-
-    block_size = [4]
-    search_area = [2]
-    step_size = [4]
-    error_function=['SSD']
-
-
-    for bs, sa, ss, ef in list(product(block_size, search_area, step_size, error_function)):
-
-        print('Block size: {0}, search area: {1}, step size: {2}, error function: {3}'.format(bs,sa,ss,ef))
-        opticalflow = block_matching_optical_flow(sequences[0], sequences[1], block_size=bs, search_area=sa, step_size=ss, error_function=ef, compensation='Backward')
-        msen, pepn = compute_msen(flow_gt, opticalflow)
-
-        print("MSEN: {}".format(msen))
-        print("PEPN: {}".format(pepn))
-
-        opticalflow_images = read_opticalflow(opticalflow_path)
-        gt_opticalflow = read_opticalflow(gt_path)
-        first_sequence = read_sequences(sequences_path)
-
-        plot_opticalflow_bm(opticalflow, first_sequence[0], title='block matching',
-                         save_path=save_path + 'BM_optflow/')
-        plot_opticalflow_gt(gt_opticalflow, first_sequence, title='GT optical flow',
-                         save_path=save_path + 'GT_optflow/', need_conversion=True)
+    # # Task 1.1
+    # create_folder(save_path + 'BM_optflow/')
+    # create_folder(save_path + 'GT_optflow/')
+    #
+    # sequences = read_sequences(sequences_path)
+    # flow_gt, flow_test = read_flow_data(gt_noc, test)
+    #
+    #
+    # block_size = [4]
+    # search_area = [2]
+    # step_size = [4]
+    # error_function=['SSD']
+    #
+    #
+    # for bs, sa, ss, ef in list(product(block_size, search_area, step_size, error_function)):
+    #
+    #     print('Block size: {0}, search area: {1}, step size: {2}, error function: {3}'.format(bs,sa,ss,ef))
+    #     opticalflow = block_matching_optical_flow(sequences[0], sequences[1], block_size=bs, search_area=sa, step_size=ss, error_function=ef, compensation='Backward')
+    #     msen, pepn = compute_msen(flow_gt, opticalflow)
+    #
+    #     print("MSEN: {}".format(msen))
+    #     print("PEPN: {}".format(pepn))
+    #
+    #     opticalflow_images = read_opticalflow(opticalflow_path)
+    #     gt_opticalflow = read_opticalflow(gt_path)
+    #     first_sequence = read_sequences(sequences_path)
+    #
+    #     plot_opticalflow_bm(opticalflow, first_sequence[0], title='block matching',
+    #                      save_path=save_path + 'BM_optflow/')
+    #     plot_opticalflow_gt(gt_opticalflow, first_sequence, title='GT optical flow',
+    #                      save_path=save_path + 'GT_optflow/', need_conversion=True)
 
     # # Task 1.2
     # farneback = farneback(sequences[0], sequences[1])
@@ -84,49 +84,49 @@ if __name__ == '__main__':
 
 
 
-    # # Task 3: CVPR challenge
-    # cameras_path = ["../datasets/AICity_data/train/S01/c001/", "../datasets/AICity_data/train/S01/c002/"]
-    # video_challenge_path = "vdo.avi"
-    # detections_challenge_path = "det/"
-    # detectors = ["det_ssd512.txt", "det_mask_rcnn.txt", "det_yolo3.txt"]
-    # groundtruth_challenge_path = "gt/gt.txt"
-    # homography_path = "calibration.txt"
-    # timestamps = [0, 1.640]
-    # framenum = [1955, 2110]
-    # fps = 10
-    # display_frames = False
-    # export_frames = False
-    # load_pkl = True
-    #
-    # detected_tracks = {}
-    # homography_cameras = {}
-    # groundtruth_list = {}
-    #
-    #
-    # for cam_num, camera in enumerate(cameras_path):
-    #     #for detector in detectors:
-    #     #print(detector)
-    #     #detections_list, _ = read_annotations_file(camera + detections_challenge_path + detector, camera + video_challenge_path)
-    #     groundtruth_list[cam_num], _ = read_annotations_file(camera + groundtruth_challenge_path, camera + video_challenge_path)
-    #     #homography_cameras[cam_num] = read_homography_matrix(camera + homography_path)
-    #
-    #     print("\nComputing tracking by overlap")
-    #     if load_pkl and os.path.exists(str(cam_num)+'.pkl'):
-    #         with open(str(cam_num)+'.pkl', 'rb') as p:
-    #             print("Reading tracks from pkl")
-    #             detected_tracks[cam_num] = pickle.load(p)
-    #             print("Tracks loaded\n")
-    #     else:
-    #
-    #         detected_tracks[cam_num] = track_objects(camera + video_challenge_path, groundtruth_list[cam_num], groundtruth_list[cam_num],
-    #                                         display=display_frames, export_frames=export_frames, idf1=False, name_pkl=str(cam_num))
-    #
-    #     # Compute mAP
-    #     #compute_mAP(groundtruth_list[cam_num], detected_tracks[cam_num])
-    #
-    # correspondences = bboxes_correspondences(groundtruth_list, timestamps, framenum, fps)
-    # #train_data, train_labels = create_dataset(groundtruth_list, timestamps, framenum, fps)
-    # #predict_bbox(train_data, train_labels)
-    # match_tracks(detected_tracks, homography_cameras, timestamps, framenum, fps, cameras_path[0] + video_challenge_path, cameras_path[1] + video_challenge_path, correspondences)
-    #
+    # Task 3: CVPR challenge
+    cameras_path = ["../datasets/AICity_data/train/S01/c001/", "../datasets/AICity_data/train/S01/c002/"]
+    video_challenge_path = "vdo.avi"
+    detections_challenge_path = "det/"
+    detectors = ["det_ssd512.txt", "det_mask_rcnn.txt", "det_yolo3.txt"]
+    groundtruth_challenge_path = "gt/gt.txt"
+    homography_path = "calibration.txt"
+    timestamps = [0, 1.640]
+    framenum = [1955, 2110]
+    fps = 10
+    display_frames = False
+    export_frames = False
+    load_pkl = False
+
+    detected_tracks = {}
+    homography_cameras = {}
+    groundtruth_list = {}
+
+
+    for cam_num, camera in enumerate(cameras_path):
+        #for detector in detectors:
+        #print(detector)
+        #detections_list, _ = read_annotations_file(camera + detections_challenge_path + detector, camera + video_challenge_path)
+        groundtruth_list[cam_num], _ = read_annotations_file(camera + groundtruth_challenge_path, camera + video_challenge_path)
+        #homography_cameras[cam_num] = read_homography_matrix(camera + homography_path)
+
+        print("\nComputing tracking by overlap")
+        if load_pkl and os.path.exists(str(cam_num)+'.pkl'):
+            with open(str(cam_num)+'.pkl', 'rb') as p:
+                print("Reading tracks from pkl")
+                detected_tracks[cam_num] = pickle.load(p)
+                print("Tracks loaded\n")
+        else:
+
+            detected_tracks[cam_num] = track_objects(camera + video_challenge_path, groundtruth_list[cam_num], groundtruth_list[cam_num],
+                                            display=display_frames, export_frames=export_frames, idf1=False, name_pkl=str(cam_num))
+
+        # Compute mAP
+        #compute_mAP(groundtruth_list[cam_num], detected_tracks[cam_num])
+
+    correspondences = bboxes_correspondences(groundtruth_list, timestamps, framenum, fps)
+    #train_data, train_labels = create_dataset(groundtruth_list, timestamps, framenum, fps)
+    #predict_bbox(train_data, train_labels)
+    match_tracks(detected_tracks, homography_cameras, timestamps, framenum, fps, cameras_path[0] + video_challenge_path, cameras_path[1] + video_challenge_path, correspondences)
+
 
